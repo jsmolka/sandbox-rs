9085d524d918ed199cd4f5a92124ba30628ab651
jsmolka <jsmolka@pm.me>
2022-11-04 14:42:20 +0100

    add icon example

---
+++ icon/build.rs
@@ -0,0 +1,12 @@
+#[cfg(windows)]
+use winres::WindowsResource;
+
+fn main() {
+    #[cfg(windows)]
+    {
+        WindowsResource::new()
+            .set_icon("assets/icon.ico")
+            .compile()
+            .unwrap();
+    }
+}
