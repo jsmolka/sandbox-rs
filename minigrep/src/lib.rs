d716768a0035556bc2d7047c1572a5d5dc1e85e4
jsmolka <jsmolka@pm.me>
2022-11-05 17:20:52 +0100

    use iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -7,15 +7,20 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Self, &'static str> {
+        args.next(); // Program
 
-        Ok(Config {
-            query: args[1].clone(),
-            filepath: args[2].clone(),
-        })
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("expected query string"),
+        };
+
+        let filepath = match args.next() {
+            Some(arg) => arg,
+            None => return Err("expected filepath"),
+        };
+
+        Ok(Config { query, filepath })
     }
 }
 
@@ -30,15 +35,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in content.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 #[cfg(test)]
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,9 +3,7 @@ use std::env;
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|error| {
+    let config = Config::build(env::args()).unwrap_or_else(|error| {
         eprintln!("Cannot parse arguments: {error}");
         std::process::exit(1);
     });
