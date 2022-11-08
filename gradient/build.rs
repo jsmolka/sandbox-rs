e4c4cb79b1d2699c64227265b6e3e8b9a21e563a
jsmolka <jsmolka@pm.me>
2022-11-08 21:31:29 +0100

    improve build

--- gradient/build.rs
+++ gradient/build.rs
@@ -1,30 +1,33 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::path::PathBuf;
 
-fn main() {
-    let target = env::var("TARGET").unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let target = env::var("TARGET")?;
     if !target.contains("pc-windows") {
-        return;
+        return Ok(());
     }
 
     if !target.contains("msvc") || !target.contains("x86_64") {
-        panic!("expected 64-bit msvc");
+        return Err("expected 64-bit msvc".into());
     }
 
-    let root = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let msvc = PathBuf::from_iter([root.as_str(), "msvc", "x64"]);
+    let root = env::var("CARGO_MANIFEST_DIR")?;
+    let deps = PathBuf::from_iter([root.as_str(), "msvc", "x64"]);
 
-    println!("cargo:rustc-link-search=all={}", msvc.display());
-    for entry in fs::read_dir(msvc).unwrap() {
-        let path = entry.unwrap().path();
+    println!("cargo:rustc-link-search=all={}", deps.display());
+    for entry in fs::read_dir(deps)? {
+        let path = entry?.path();
         if let Some(filename) = path.file_name() {
             let filename = filename.to_str().unwrap();
             if !filename.ends_with(".dll") {
                 continue;
             }
             let dest = PathBuf::from_iter([root.as_str(), filename]);
-            fs::copy(&path, dest.as_path()).unwrap();
+            fs::copy(&path, dest.as_path())?;
         }
     }
+
+    Ok(())
 }
